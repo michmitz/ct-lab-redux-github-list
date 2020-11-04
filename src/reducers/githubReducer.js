6b7270d9533760b330914fbbdfc541ae028a5f20
michmitz <stermitzm@gmail.com>
2020-11-03 19:42:55 -0800

    Setup redux

---
+++ src/reducers/githubReducer.js
@@ -0,0 +1,11 @@
+const initialState = {
+  username: '',
+  followers: 0,
+  repolist: []
+}
+
+export default function reducer(state = initialState, action) {
+  switch(action.type) {
+    default: return state;
+  }
+}
